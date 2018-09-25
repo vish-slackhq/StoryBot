9e32ce0d529c84087050df903944fce2e9764943
smocky <dsmock@gmail.com>
2018-09-25 13:18:57 -0400

    bye felicia

--- test.js
+++
@@ -1,6 +0,0 @@
-let str = '/mediasource thingy';
-let test = str.match(/(\/.*(?=\s)) .*/);
-console.log('test:',test);
-console.log('test[0]:',test[0]);
-console.log('test[1]:',test[1]);
-console.log('test[2]:',test[2]);
